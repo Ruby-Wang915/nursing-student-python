40309471c6e9e53f2319c810a1687a8dc5e3de4f
Ruby-Wang915 <pengyun0915@gmail.com>
2025-11-20 21:58:44 +0800

    Add number guessing game functionality

--- random_games/guess_number.py
+++ random_games/guess_number.py
@@ -1,1 +1,15 @@
 # 終極密碼 (猜數字遊戲)
+import random as rd
+Ans=rd.randint(1,100)
+
+while True:
+    guess=eval(input())
+    if guess<1 or guess>100:
+        print("範圍錯誤")
+    elif guess>Ans:
+        print("太大")
+    elif guess<Ans:
+        print("太小")
+    else:
+        print("答對")
+        break
