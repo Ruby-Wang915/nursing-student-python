a156930042f9809ccc3610f700a21f7dbd9966f5
Ruby-Wang915 <pengyun0915@gmail.com>
2025-11-24 12:12:27 +0800

    Refactor odd and even sum calculations

--- loop/count 1-100.py
+++ loop/count 1-100.py
@@ -1,17 +1,18 @@
 #從1加到100 只加奇數
-x=0
+odd=0
 for i in range(1,100,2):
-    x+=i
-print('1+3+5+...+99=',x)
+    odd+=i
+print('1+3+5+...+99=',odd)
 
 #從1加到100 只加偶數
-y=0
+even=0
 for i in range(2,101,2):
-    y+=i
-print('2+4+6+...+100=',y)
+    even+=i
+print('2+4+6+...+100=',even)
 
 #從1加到100
 total=0
 for i in range(1,101):
     total+=i
 print('1+2+3+...+100=',total)
+
