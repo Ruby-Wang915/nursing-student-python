e709b4c43642744761bf4ad806e936dfbd3a7530
Ruby-Wang915 <pengyun0915@gmail.com>
2025-12-01 21:46:57 +0800

    Add diamond pattern printing in diamond 02.py

---
+++ loop/for/diamond 02.py
@@ -0,0 +1,21 @@
+n=eval(input('enter layar:'))
+
+for i in range(1,n+1):
+    for j in range(1,i+1):   
+        print(j,end=" ") 
+    print("")
+
+for i in range(n-1,0,-1):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
+
+'''
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3
+1 2
+1
+'''
